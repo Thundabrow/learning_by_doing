0aba53d5eb5745ea491126202f49ca262b8f1db5
David Jarrett <dave.jarrett@gmail.com>
2015-10-15 12:56:45 -0600

    Added last lyric. [#105164886]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -15,7 +15,8 @@ end
 if bottles <= 1
   puts "#{bottles} bottle of beer on the wall, #{bottles} bottle of beer.\n"
   puts "Take one down and pass it around, no more bottles of beer on the wall.\n"
-  puts "No more bottles of beer on the wall, no more bottles of beer."
+  puts "No more bottles of beer on the wall, no more bottles of beer.\n"
+  puts "Go to the store and buy some more, 99 bottles of beer on the wall."
   return
 end
 end
