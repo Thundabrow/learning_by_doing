a3301942fae4f94730858bedf6a1bc0d7d4597b1
David Jarrett <dave.jarrett@gmail.com>
2015-10-29 17:59:32 -0600

    Added pluralize method. [#105164886]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,24 +1,25 @@
 #!/usr/bin/env ruby
-
+def pluralize(number, word = 'bottle')
+  if number >= 2
+    word = word + 's'
+    return word
+  end
+  else
+    word
+  end
 def beer(bottles)
-
-if bottles > 2
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\n"
-  puts "Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.\n"
-  beer(bottles - 1)
-end
-if bottles == 2
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\n"
-  puts "Take one down and pass it around, #{bottles - 1} bottle of beer on the wall.\n"
-  beer (bottles - 1)
-end
-if bottles <= 1
-  puts "#{bottles} bottle of beer on the wall, #{bottles} bottle of beer.\n"
-  puts "Take one down and pass it around, no more bottles of beer on the wall.\n"
-  puts "No more bottles of beer on the wall, no more bottles of beer.\n"
-  puts "Go to the store and buy some more, 99 bottles of beer on the wall."
+  if bottles <= 1
+    puts "#{bottles} #{pluralize(bottles)} of beer on the wall, #{bottles} #{pluralize(bottles)} of beer.\n"
+    puts "Take one down and pass it around, no more bottles of beer on the wall.\n"
+    puts "No more bottles of beer on the wall, no more bottles of beer.\n"
+    puts "Go to the store and buy some more, 99 bottles of beer on the wall."
   return
-end
+  end
+
+else
+  puts "#{bottles} #{pluralize(bottles)} of beer on the wall, #{bottles} #{pluralize(bottles)} of beer.\n"
+  puts "Take one down and pass it around, #{bottles - 1} #{pluralize(bottles)} of beer on the wall.\n"
+  beer(bottles - 1)
 end
 
 beer(99)
